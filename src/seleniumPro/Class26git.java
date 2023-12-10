d096eff241e73059d0faeb44b88d70b6ade4c87f
digvijay <digvijaypatil7295@gmail.com>
2023-12-10 23:45:00 +0530

    second commit

---
+++ src/seleniumPro/Class26git.java
@@ -0,0 +1,21 @@
+package seleniumPro;
+
+import java.util.ArrayList;
+
+public class Class26git {
+
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Digvijay");
+		a.add("Patil");
+		a.add("selenium");
+		a.add("Academy");
+		a.add("exposure");
+		
+		a.remove(2);
+		
+		System.out.println(a.get(3));
+
+	}
+
+}
